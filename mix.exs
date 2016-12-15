5d38d9eae37b85e16b9d11698071bf6c6e3e1bb7
Joseph Miller <joff@joff.codes>
2016-12-15 17:03:03 +1100

    Fix envs for linter apps

--- mix.exs
+++ mix.exs
@@ -16,16 +16,20 @@ defmodule QBot.Mixfile do
      mod: {QBot, []}]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "spec/support"]
+  defp elixirc_paths(_),     do: ["lib"]
+
   defp deps do
     [
-      {:ex_aws, "~> 1.0.0-rc4"},
-      {:distillery, "~> 1.0"},
+      {:ex_aws, "1.0.0-rc4"},
+      {:distillery, "1.0"},
       {:logger_papertrail_backend, "~> 0.1.1"},
-      {:rollbax, "~> 0.8"},
-      {:dogma, "0.1.13", only: [:dev, :test]},
-      {:credo, "~> 0.5.3", only: [:dev, :test]},
-      {:espec, "~> 1.2", only: :test,  app: false},
-      {:apex, "~>0.6.0"},
+      {:rollbax, "0.8"},
+      {:dogma, "0.1.13", only: [:dev, :test, :lint]},
+      {:credo, "0.5.3", only: [:dev, :test, :lint]},
+      {:espec, "1.2", only: :test,  app: false},
+      {:apex, "0.6"},
       {:codeclimate_credo, git: "https://github.com/fazibear/codeclimate-credo",
                            branch: "master", only: [:dev, :test] },
     ]
