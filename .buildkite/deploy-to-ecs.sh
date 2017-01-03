35ae87ee7020731a2bda84fe1ac5608d57c680ef
Joseph Miller <joff@joff.codes>
2017-01-03 17:52:51 +1100

    Don't set when you mean get :p

--- .buildkite/deploy-to-ecs.sh
+++ .buildkite/deploy-to-ecs.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 export ECS_CLUSTER=$(buildkite-agent meta-data get "ecs-cluster")
-export DOCKER_TAG_PREFIX=$(buildkite-agent meta-data set "docker-tag-prefix")
+export DOCKER_TAG_PREFIX=$(buildkite-agent meta-data get "docker-tag-prefix")
 
 docker-compose run --rm deployment ./deploy_ecs.rb
