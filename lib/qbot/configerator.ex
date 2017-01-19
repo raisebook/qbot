428881845c3e796de3c81fc06fb1a9ab2c3486da
Myles Eftos <myles@madpilot.com.au>
2017-01-19 15:45:09 +1100

    Linter hates semi-colons

--- lib/qbot/configerator.ex
+++ lib/qbot/configerator.ex
@@ -33,7 +33,7 @@ defmodule QBot.Configerator do
   end
 
   defp fetch_resource(queue) do
-    :timer.sleep(100);
+    :timer.sleep(100)
 
     {:ok, %{body: %{resource: resource}}} = cf_stack
     |> ExAws.Cloudformation.describe_stack_resource(queue)
